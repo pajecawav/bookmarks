20dba3f169c0f58a72c1a10eb1900a7cbba936b5
pajecawav <pajecawav@gmail.com>
2021-01-07 16:57:57 +0300

    Remove CORS middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,22 +1,8 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import api
 from app.config import settings
 
 app = FastAPI()
-
-origins = [
-    "*",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 
 app.include_router(api.api_router, prefix=settings.API_ROUTE)
