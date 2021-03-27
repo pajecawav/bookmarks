f53a094fa7745dac9dd3e8ee7bd26e15bb075bf6
pajecawav <pajecawav@gmail.com>
2021-03-27 21:15:31 +0300

    Add NUM_WORKERS environment variable

--- backend/start.sh
+++ backend/start.sh
@@ -2,4 +2,4 @@
 
 alembic upgrade head
 python -m app.db.init_db
-uvicorn --workers 4 --host 0.0.0.0 --root-path "/api" app.main:app $@
+uvicorn --workers ${NUM_WOKERS:-1} --host 0.0.0.0 --root-path "/api" app.main:app $@
