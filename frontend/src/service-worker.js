c0a7580b6ed0a0c91da098c0774e7b19d9a2f088
pajecawav <pajecawav@gmail.com>
2021-03-30 20:34:18 +0300

    Frontend: remove unused router from service worker

--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -1,10 +1,8 @@
 /* eslint-disable no-restricted-globals */
 
 import { clientsClaim } from "workbox-core";
-import { ExpirationPlugin } from "workbox-expiration";
-import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching";
+import { createHandlerBoundToURL, precacheAndRoute } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
-import { StaleWhileRevalidate } from "workbox-strategies";
 
 clientsClaim();
 
@@ -30,15 +28,6 @@ registerRoute(({ request, url }) => {
 
     return true;
 }, createHandlerBoundToURL(process.env.PUBLIC_URL + "/index.html"));
-
-registerRoute(
-    ({ url }) =>
-        url.origin === self.location.origin && url.pathname.endsWith(".png"),
-    new StaleWhileRevalidate({
-        cacheName: "images",
-        plugins: [new ExpirationPlugin({ maxEntries: 50 })],
-    })
-);
 
 self.addEventListener("message", (event) => {
     if (event.data && event.data.type === "SKIP_WAITING") {
